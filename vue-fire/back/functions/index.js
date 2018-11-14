217e25a78d48cdd91bad847ff4fa1d0531001492
Enrique <enrikiko_91@hotmail.com>
2018-11-14 22:52:30 +0100

    'sc'

--- vue-fire/back/functions/index.js
+++ vue-fire/back/functions/index.js
@@ -2,9 +2,13 @@ const functions = require('firebase-functions');
 const express = require("express")
 const admin = require("firebase-admin")
 const fs = require('fs');
+const clientIp  = require('client-id')
 admin.initializeApp();
 const app1 = express()
 app1.get('/eureka', (req,res) => {
+  admin.database().ref('Cvdownloaded/IPs').push().set({
+  		"stampTime"	: 	getTime(),
+  		"val"		: 	clientIp(req)		})
   admin.database().ref('Cvdownloaded/val').once("value", snap => {
       var num = snap.val()
       console.log(num)
@@ -16,4 +20,19 @@ app1.get('/eureka', (req,res) => {
 const api1 = functions.https.onRequest(app1)  
 module.exports = {
   api1
+}
+
+
+function getTime() {
+  var miliSec,sec,min,hour,date,month,year,currentDate;
+  currentDate =  new Date();
+  miliSec = currentDate.getMilliseconds();
+  hour = currentDate.getHours();
+  min = currentDate.getMinutes();
+  sec = currentDate.getSeconds();
+  date = currentDate.getDate();
+  month = currentDate.getMonth();
+  year = currentDate.getFullYear();
+  var txt = "Time: " + hour + ":" + min + ":" + sec + ":" + miliSec + " "+ date + "-" +(month + 1) + "-" + year;
+  return txt;
 }
