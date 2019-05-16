65d4cc12e44f9fed853443e11498c18c5aef83c0
enrikiko <enrikiko_91@hotmail.com>
2019-05-16 21:27:53 +0200

    shell

---
+++ shellApi/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const myDevice = require('./users');
+const cors = require('cors');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const app = express();
+app.enable('trust proxy')
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
+app.use(express.urlencoded()) // middleware Bodyparse
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const { exec } = require('child_process');
+
+app.post("/", function(req, res, next) { //OK
+  var request={}
+  request.url=req.get('host')+req.originalUrl
+  request.body=JSON.stringify(req.body)
+  request.ip=req.i
+  console.log(JSON.stringify(request));
+  })
+app.post('/*', function(req, res){
+  res.status(200).json(request)
+});
+
+app.get("/", function(req, res, next) { //OK
+  var request={}
+  request.url=req.get('host')+req.originalUrl
+  request.body=JSON.stringify(req.body)
+  request.ip=req.i
+  console.log(JSON.stringify(request));
+  })
+
+app.get('/*', function(req, res){
+  res.status(200).json(request)
+});
+
+http.listen(3000, function () {
+    log('Servidor activo en http://localhost:3000');
+  })
