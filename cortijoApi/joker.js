2a24e5af200a296282bf8d6c5a0a5644eab88bac
enrikiko <enrikiko_91@hotmail.com>
2019-05-31 18:45:09 +0200

    auto-commit

--- cortijoApi/joker.js
+++ cortijoApi/joker.js
@@ -3,12 +3,12 @@ module.exports={
      switchStatus: (ip, status) => {
        log("IP:", ip)
        log("STATUS:", status)
-       request("http://"+ip+"/status/"+status, (err, res, body) => {
+       request("http://"+ip+"/"+status, (err, res, body) => {
          if (err) {
            return console.log(err);
          }
        });
      }
 
-     
+
 }
