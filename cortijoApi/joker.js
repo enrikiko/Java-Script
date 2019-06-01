71d77edacc0ad797c02f8901ea5d68957a7c4f8c
enrikiko <enrikiko_91@hotmail.com>
2019-06-01 19:29:06 +0200

    auto-commit

--- cortijoApi/joker.js
+++ cortijoApi/joker.js
@@ -7,16 +7,19 @@ module.exports={
        console.log("IP:", ip)
        console.log("STATUS:", status)
        console.log("http://"+ip+"/"+status);
-       var response = request("http://"+ip+"/"+status, (err, res, body) => {
+       var certain = "certain"
+       var response = await request("http://"+ip+"/"+status, (err, res, body) => {
          if (err) {
            console.log(err);
            return err
          } else if(res){
            //console.log(res);
+           certain=res
            return res
          }
        });
-       console.log(response)
+       console.log("response: " + response)
+       console.log("certain: " + certain);
        return response;
      },
     readLog: () => {
