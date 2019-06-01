0cb4a6c12fa82a53d0f4779e7b555d521eb0c3f6
enrikiko <enrikiko_91@hotmail.com>
2019-06-01 18:45:50 +0200

    auto-commit

--- cortijoApi/deviceMock/scs/request.sh
+++ cortijoApi/deviceMock/scs/request.sh
@@ -1,8 +1,8 @@
 ID="id"
 NAME="Enrique"
-IP="192.168.99.100:8000"
+IP="192.168.1.50:8000"
 STATUS="true"
-SERVERIP="192.168.99.100:80"
+SERVERIP="192.168.1.50:3371"
 DESCRIPTION="A_text_is_any_object_that_can_be_read,_including:_Documents:_Religious_text,_a_writing_that_a_religious_tradition_considers_to_be_sacred"
 RES=$(curl "$SERVERIP"/device/new/"$NAME/$STATUS/$IP")
 echo $RES
