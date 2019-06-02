4fef1fcb73126c68cd4390f454c6648b9f04a921
enrikiko <enrikiko_91@hotmail.com>
2019-06-02 17:33:29 +0200

    auto-commit

--- cortijoApi/deviceMock/scs/request.sh
+++ cortijoApi/deviceMock/scs/request.sh
@@ -1,8 +1,7 @@
-ID="id"
 NAME=$mock_name
 IP="192.168.1.50"
 STATUS="on"
 SERVERIP="192.168.1.50:3371"
-echo "$SERVERIP"/new/"$NAME/$STATUS/$IP$mock_port"
-RES=$(curl "$SERVERIP"/new/"$NAME/$STATUS/$IP$mock_port")
+echo "$SERVERIP"/new/"$NAME/$STATUS/$IP:$mock_port"
+RES=$(curl "$SERVERIP"/new/"$NAME/$STATUS/$IP:$mock_port")
 echo $RES
--- cortijoApi/redeploy.sh
+++ cortijoApi/redeploy.sh
@@ -1,4 +1,4 @@
-docker rm -f mock cortijo-api-2
+docker rm -f mock mock1 mock3 cortijo-api-2
 git pull
 docker build -t cortijo-api-2 .
 docker run -d -p 3371:3000 --name cortijo-api-2 cortijo-api-2
