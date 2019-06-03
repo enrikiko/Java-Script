728ccf9b92a623a237b13c5505bf86bc737cad04
enrikiko <enrikiko_91@hotmail.com>
2019-06-03 08:47:04 +0200

    auto-commit

--- cortijoApi/deviceMock/scs/request.sh
+++ cortijoApi/deviceMock/scs/request.sh
@@ -1,7 +1,6 @@
-NAME=$mock_name
 IP="192.168.1.50"
 STATUS="true"
 SERVERIP="192.168.1.50:3371"
-echo "$SERVERIP"/new/"$NAME/$STATUS/$IP:$mock_port"
-RES=$(curl "$SERVERIP"/new/"$NAME/$STATUS/$IP:$mock_port")
+echo "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port"
+RES=$(curl "$SERVERIP"/new/"$mock_name/$STATUS/$IP:$mock_port")
 echo $RES
